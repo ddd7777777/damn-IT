82cad31b36d2c3cdf76e77dff5e89a4622e45b5e
qunyangqq <147961425+qunyangqq@users.noreply.github.com>
2024-04-19 11:05:08 +0800

    damn C language compiler-gq

---
+++ damn C language compiler-gq.cpp
@@ -0,0 +1,124 @@
+void sentence_if()
+{
+    if(flag == 0)
+    {
+        return;
+    }
+    if(!strcmp(str[i], "if"))
+    {
+        printf("<if语句>→<if关键字> (<表达式>)<复合语句><N>\n");
+        key_if();
+        if(!strcmp(str[i], "("))
+        {
+            i++;
+            express();
+            if(!strcmp(str[i], ")"))
+            {
+                i++;
+                sentence_multiple();
+                sentence_if_else();
+            }
+            else
+            {
+                flag = 0;
+            }
+        }
+        else
+        {
+            flag = 0;
+        }
+    }
+    else
+    {
+        flag = 0;
+    }
+}
+void sentence_if_else()
+{
+    if(flag == 0)
+    {
+        return;
+    }
+    if(!strcmp(str[i], "else"))
+    {
+        printf("<N>→<else关键字><复合语句>\n");
+        key_else();
+        sentence_multiple();
+    }
+    else
+    {
+        printf("<N>→<null>\n");
+    }
+}
+void sentence_while()
+{
+    printf("<while语句>→<while关键字> (<表达式>)<复合语句>\n");
+    key_while();
+    if(!strcmp(str[i], "("))
+    {
+        i++;
+        express();
+        if(!strcmp(str[i], ")"))
+        {
+            i++;
+            sentence_multiple();
+        }
+        else
+        {
+            flag = 0;
+        }
+    }
+    else
+    {
+        flag = 0;
+    }
+    if(flag == 0)
+    {
+        return;
+    }
+}
+void sentence_for()
+{
+    printf("<for语句>→<for关键字> (<表达式>;<表达式>;<表达式>) <复合语句>\n");
+    key_for();
+    if(!strcmp(str[i], "("))
+    {
+        i++;
+        express();
+        if(!strcmp(str[i], ";"))
+        {
+            i++;
+            express();
+            if(!strcmp(str[i], ";"))
+            {
+                i++;
+                express();
+                if(!strcmp(str[i], ")"))
+                {
+                    i++;
+                    sentence_multiple();
+                }
+                else
+                {
+                    flag = 0;
+                }
+            }
+            else
+            {
+                flag = 0;
+            }
+        }
+        else
+        {
+            flag = 0;
+        }
+    }
+    else
+    {
+        flag = 0;
+    }
+    if(flag == 0)
+    {
+        return;
+    }
+}
